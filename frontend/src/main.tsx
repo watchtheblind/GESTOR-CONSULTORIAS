9c0e7c079c33f4c6427a9674cb965f2b898bd24f
Luis Monasterios <ljmm2k2@gmail.com>
2025-07-30 18:21:45 -0400

    chore: add dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {Provider} from './components/ui/provider'
+import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>  
+  </Provider>
+  
+ 
   </StrictMode>,
 )
